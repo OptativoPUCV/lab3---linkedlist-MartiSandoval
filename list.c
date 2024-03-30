36d3ba5dcede4e80511b1e639bd98ac2ac755f87
Your Name <you@example.com>
2024-03-30 23:37:29 +0000

    new try

--- list.c
+++ list.c
@@ -91,7 +91,6 @@ void pushCurrent(List * list, void * data) {
     nuevo->prev = list->current;
     list->current->next->prev = nuevo;
     list->current->next = nuevo;
-
   }
   else {
     nuevo->prev = list->current;
@@ -112,12 +111,27 @@ void * popBack(List * list) {
     return popCurrent(list);
 }
 
-void * popCurrent(List * list) {
-  /*Node* aux = list->current;
-  if (list->current == list->head) {
+void *popCurrent(List * list) {
+  Node* aux = list->current;
+  if (list->head == NULL) return NULL;
+    
+  else if (list->current == list->head) {
+    void * dato = list->head->data;
     list->head = list->head->next;
     list->head->prev = NULL;  
-  }*/
+    return dato;
+  }
+  else if (list->current->next == NULL) { 
+    void* dato = list->tail->data;
+    list->tail = list->current->prev;
+    list->current->prev->next = NULL;
+    
+  }
+  else {
+    void* dato = list->current->data;
+    list->current->prev->next = list->current->next;
+    list->current->next->prev = list->current->prev;
+  }
   
     return NULL;
 }
